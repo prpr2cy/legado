93fcf135a4bf9bc6c614cd6be628e69b7a723caf
prpr2cy <qiling200@sina.cn>
2026-01-12 12:57:02 +0800

    Update 1 file: BookSourceEditActivity.kt

--- app/src/main/java/io/legado/app/ui/book/source/edit/BookSourceEditActivity.kt
+++ app/src/main/java/io/legado/app/ui/book/source/edit/BookSourceEditActivity.kt
@@ -7,6 +7,7 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewGroup
 import android.widget.EditText
 import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
