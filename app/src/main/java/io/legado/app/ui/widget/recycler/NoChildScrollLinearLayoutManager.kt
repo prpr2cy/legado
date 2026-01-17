1f88caeb65485da5a33437821202dc63db790809
prpr2cy <qiling200@sina.cn>
2026-01-17 12:44:38 +0800

    Update 1 file: NoChildScrollLinearLayoutManager.kt

--- app/src/main/java/io/legado/app/ui/widget/recycler/NoChildScrollLinearLayoutManager.kt
+++ app/src/main/java/io/legado/app/ui/widget/recycler/NoChildScrollLinearLayoutManager.kt
@@ -104,30 +104,20 @@ class NoChildScrollLinearLayoutManager @JvmOverloads constructor(
         // 计算光标需要滚动的距离
         val neededScrollY = cursorBottomInWindow - keyboardTopInwindow
 
-        val originalScrollY = edit.scrollY
-        edit.scrollTo(0, Int.MAX_VALUE)
-        val maxScrollY = edit.scrollY
-        edit.scrollTo(0, originalScrollY)
-
-        // 计算EditText剩余的滚动空间
-        val remainingScrollY = maxScrollY - originalScrollY
-
-        // 还有内部滚动空间，先滚动EditText
-        if (remainingScrollY > 0) {
-            val neededScrollInside = min(neededScrollY, remainingScrollY)
-            edit.scrollBy(0, neededScrollInside)
-        }
+        // 先尝试滚动EditText
+        val oldScrollY = edit.scrollY
+        edit.scrollBy(0, neededScrollY)
+        // EditText实际滚动的的距离
+        val alreadyScrolled = edit.scrollY - oldScrollY 
 
         // 滚动完EditText后，还被遮挡再滚动RecyclerView
-        /*
-        if (neededScrollY > remainingScrollY) {
+        if (neededScrollY > alreadyScrolled) {
             val neededScrollRv = editBottomInWindow - keyboardTopInwindow
             if (neededScrollRv > 0) {
                 rv.stopScroll()
                 rv.scrollBy(0, neededScrollRv)
             }
         }
-        */
     }
 
     /**
