0f1e312b39e07764f380b0665ec966818c146cad
prpr2cy <qiling200@sina.cn>
2026-02-02 02:23:32 +0800

    Update 1 file: NumberPickerDialog.kt

--- app/src/main/java/io/legado/app/ui/widget/number/NumberPickerDialog.kt
+++ app/src/main/java/io/legado/app/ui/widget/number/NumberPickerDialog.kt
@@ -52,15 +52,10 @@ class NumberPickerDialog(context: Context) {
 
     fun show(callBack: ((value: Int) -> Unit)?) {
         builder.setPositiveButton(R.string.ok) { _, _ ->
-            numberPicker?.let { np ->
-                np.clearFocus()
-                np.hideSoftInput()
-                np.post {
-                    if (np.value < np.minValue) {
-                        np.value = np.minValue
-                    }
-                    callBack?.invoke(np.value)
-                }
+            numberPicker?.let {
+                it.clearFocus()
+                it.hideSoftInput()
+                callBack?.invoke(it.value)
             }
         }
         builder.setNegativeButton(R.string.cancel, null)
