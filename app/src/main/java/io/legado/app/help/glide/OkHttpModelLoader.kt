8f2db6b49d688cbb80124a118c9cf968e4c68127
prpr2cy <qiling200@sina.cn>
2026-03-26 23:12:52 +0800

    Update 2 files: OkHttpModelLoader.kt, OkHttpStreamFetcher.kt

--- app/src/main/java/io/legado/app/help/glide/OkHttpModelLoader.kt
+++ app/src/main/java/io/legado/app/help/glide/OkHttpModelLoader.kt
@@ -4,16 +4,17 @@ import com.bumptech.glide.load.Option
 import com.bumptech.glide.load.Options
 import com.bumptech.glide.load.model.GlideUrl
 import com.bumptech.glide.load.model.ModelLoader
+import io.legado.app.data.entities.BaseSource
 import io.legado.app.help.source.SourceHelp
 import io.legado.app.model.analyzeRule.AnalyzeUrl
 import io.legado.app.utils.isAbsUrl
-
 import java.io.InputStream
 
 object OkHttpModelLoader : ModelLoader<GlideUrl?, InputStream?> {
 
     val loadOnlyWifiOption = Option.memory("loadOnlyWifi", false)
     val sourceOriginOption = Option.memory<String>("sourceOrigin")
+    val sourceOption = Option.memory<BaseSource>("source")
 
     override fun buildLoadData(
         model: GlideUrl,
@@ -29,6 +30,7 @@ object OkHttpModelLoader : ModelLoader<GlideUrl?, InputStream?> {
         if (cacheKey.isAbsUrl()) {
             modelWithHeader = AnalyzeUrl(cacheKey, source = source).getGlideUrl()
         }
+        options.set(sourceOption, source)
         return ModelLoader.LoadData(modelWithHeader, OkHttpStreamFetcher(modelWithHeader, options))
     }
 
