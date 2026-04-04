d4c2c45aee888bf1cf4651524df2e2ba98634e3b
prpr2cy <qiling200@sina.cn>
2026-04-04 17:42:12 +0800

    Update 1 file: JsonExtensions.kt

--- app/src/main/java/io/legado/app/utils/JsonExtensions.kt
+++ app/src/main/java/io/legado/app/utils/JsonExtensions.kt
@@ -1,6 +1,6 @@
 package io.legado.app.utils
 
-import com.google.gson.GsonBuilder
+import com.google.gson.Gson
 import com.google.gson.JsonElement
 import com.google.gson.JsonParser
 import com.google.gson.ToNumberPolicy
