8b793baadf53a88f952b25549fb972200893eaae
prpr2cy <qiling200@sina.cn>
2026-08-23 15:11:41 +0800

    Update 1 file: BookshelfViewModel.kt

--- app/src/main/java/io/legado/app/ui/main/bookshelf/BookshelfViewModel.kt
+++ app/src/main/java/io/legado/app/ui/main/bookshelf/BookshelfViewModel.kt
@@ -87,7 +87,7 @@ class BookshelfViewModel(application: Application) : BaseViewModel(application) 
                 }
                 if (source == null) {
                     hasBookUrlPattern.forEach { bookSource ->
-                        if (bookUrl.matches(bookSource.bookUrlPattern!!.toRegex())) {
+                        if (bookUrl.contains(bookSource.bookUrlPattern!!.toRegex())) {
                             source = bookSource
                             return@forEach
                         }
